755ede98590c0afa8e2557020ae5369ad02c4321
conghoang123 <namlunoy@gmail.com>
2015-03-26 13:08:09 +0700

    asdf

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,46 @@
+﻿#include "HelloWorldScene.h"
+#include "ui/CocosGUI.h"
+
+
+USING_NS_CC;
+#include <vector>
+using namespace std;
+
+Scene* HelloWorld::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = HelloWorld::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+	if (!Layer::init())
+		return false;
+	Size screenSize = Director::getInstance()->getVisibleSize();
+
+	return true;
+}
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+	return;
+#endif
+
+	Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+	exit(0);
+#endif
+}
