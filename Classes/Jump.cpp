e80e3eb4ee3c5492488684405b761eda9660d3ae
HoangDang0306 <hoangdang0306@gmail.com>
2015-03-30 10:31:45 +0700

    afaf

--- Classes/Jump.cpp
+++ Classes/Jump.cpp
@@ -21,22 +21,15 @@ bool Jump::init()
 	//Background
 	auto background = Sprite::create("Background.jpg");
 	background->setAnchorPoint(Vec2(0.5, 0.5));
+	background->setScale(0.5);
 	background->setPosition(screenSize.width / 2, screenSize.height / 2);
 	this->addChild(background, 0);
 
-	//Title
-	auto text = Label::createWithTTF("Jumpy", "fonts/arial.ttf", 30);
-	text->setPosition(Point(screenSize.width / 2, (screenSize.height / 5) * 4));
-	this->addChild(text);
-	//float height = 100;
-	
-	//auto ninjaSprite = Sprite::create("Ninja2.png");
-	////Vec2 currentPos = ninjaSprite->getPosition();
-	//ninjaSprite->setPosition(screenSize.width / 2, screenSize.height / 2);
-	//Vec2 currentPos = ninjaSprite->getPosition();
-	//auto jump = JumpTo::create(2, Vec2(currentPos), height, 1);
-	//ninjaSprite->runAction(jump);
-	//this->addChild(ninjaSprite);
+	//Ninja
+	float height = 100;
+	auto ninjaSprite = Sprite::create("Ninja2.png");
+	ninjaSprite->setPosition(screenSize.width / 2, screenSize.height / 2);
+	this->addChild(ninjaSprite);
 
 	//Touch
 	auto touchListener = EventListenerTouchOneByOne::create();
@@ -44,16 +37,15 @@ bool Jump::init()
 	touchListener->onTouchEnded = CC_CALLBACK_2(Jump::onTouchEnded, this);
 	_eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
 	
-
 	return true;
 }
 
-void Jump::jump(Sprite *sprite, float height, float duration)
-{
-	Vec2 currentPos = sprite->getPosition();
-	auto jump = JumpTo::create(2, Vec2(currentPos), height, 1);
-	sprite->runAction(jump);
-}
+//void Jump::jump(Sprite *sprite, float height)
+//{
+//	Vec2 currentPos = sprite->getPosition();
+//	auto jump = JumpTo::create(2, Vec2(currentPos), height, 1);
+//	sprite->runAction(jump);
+//}
 
 bool Jump::onTouchBegan(Touch *touch, Event *unused_event)
 {
@@ -67,15 +59,5 @@ void Jump::onTouchMoved(Touch *touch, Event *unused_event)
 
 void Jump::onTouchEnded(Touch *touch, Event *unused_event)
 {
-	float height = 100;
-	Size screenSize = Director::getInstance()->getVisibleSize();
-	auto ninjaSprite = Sprite::create("Ninja2.png");
-	ninjaSprite->setPosition(screenSize.width / 2, screenSize.height / 2);
-
-	/*auto currentPos = ninjaSprite->getPosition();
-	auto jump = JumpTo::create(2, Vec2(screenSize.width / 2, screenSize.height / 2), height, 1);
-	ninjaSprite->runAction(jump);*/
-	jump(ninjaSprite, height, 0.5);
-	this->addChild(ninjaSprite);
-	
+	//jump(ninjaSprite, height);	
 }
