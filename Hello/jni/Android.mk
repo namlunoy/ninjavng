64dea57449ff9597665e17220d78df236435012b
conghoang123 <namlunoy@gmail.com>
2015-04-09 09:38:39 +0700

    Them File Android.ml

--- Hello/jni/Android.mk
+++ Hello/jni/Android.mk
@@ -12,17 +12,27 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
+                   ../../Classes/Layers/Jump/GamePlayLayer.cpp \
+                   ../../Classes/Layers/Jump/JumpLayer.cpp \
+                   ../../Classes/Layers/BanSung/BanSungLayer.cpp \
+                   ../../Classes/Layers/PhiTieu/PhiTieuHUDLayer.cpp \
+                   ../../Classes/Layers/PhiTieu/PhiTieuLayer.cpp \
+                   ../../Classes/Models/PhiTieu/Ninja.cpp \
+                   ../../Classes/Models/PhiTieu/NinjaModel.cpp \
+                   ../../Classes/Models/Jump/Background.cpp \
+                   ../../Classes/Models/Jump/Ninja_D.cpp \
+                   ../../Classes/Models/Jump/Pillar.cpp \
+                   ../../Classes/Models/PhiTieu/Shuriken.cpp \
                    ../../Classes/Scenes/HelloWorldScene.cpp \
                    ../../Classes/Scenes/JumpScene.cpp \
+                   ../../Classes/Scenes/BanSungScene.cpp \
+                   ../../Classes/Scenes/ChemScene.cpp \
                    ../../Classes/Scenes/PhiTieuScene.cpp \
-                   ../../Classes/Layers/JumpLayer.cpp \
-                   ../../Classes/Layers/PhiTieuLayer.cpp \
-                   ../../Classes/Layers/PhiTieuHUDLayer.cpp \
-                   ../../Classes/Models/Ninja.cpp \
-                   ../../Classes/Models/Shuriken.cpp \
+                   ../../Classes/Scenes/TranhNeScene.cpp \
                    ../../Classes/Utility/Config.cpp \
-                   ../../Classes/Utility/XHelper.cpp
-                   
+                   ../../Classes/Utility/XHelper.cpp \
+                   ../../Classes/Utility/Tags.cpp \
+                   ../../Classes/Utility/Definition.cpp
          
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
@@ -32,6 +42,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
+
 
 # _COCOS_LIB_ANDROID_BEGIN
 # _COCOS_LIB_ANDROID_END
