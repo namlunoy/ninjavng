5e1134e5ff3a9b823332673bd0bb7ace84d50a65
conghoang123 <namlunoy@gmail.com>
2015-04-09 17:03:56 +0700

    Bo project android cho nhe

--- Hello/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/Utility/Tags.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/Utility/Tags.o: \
- jni/../../Classes/Utility/Tags.cpp \
- E:/cocos2dx/NDK/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream
-
-E:/cocos2dx/NDK/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
