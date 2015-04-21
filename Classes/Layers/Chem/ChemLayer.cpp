3bc3a695e01bb43c8b1ebbbc68f8601d1225b499
doloc <dolocnd@gmail.com>
2015-04-21 16:45:58 +0700

    Sua file chemlayer va chemscene

--- Classes/Layers/Chem/ChemLayer.cpp
+++ Classes/Layers/Chem/ChemLayer.cpp
@@ -7,6 +7,7 @@
 
 #include "ChemLayer.h"
 #include"SimpleAudioEngine.h"
+#include"GameOverScene.h"
 
 USING_NS_CC;
 
@@ -80,12 +81,24 @@ bool ChemLayer::init() {
 	// Gọi tới hàm gameLogic , hàm này có nhiệm vụ tạo ra đám quái với thời gian 1 giây 1 quái
 	this->schedule( schedule_selector(ChemLayer::gameLogic), 1.0 );
 
-	//-----------------  Touch event ------------
+	//Tạo đối tượng truyền tải thông tin của các sự kiện
+	auto dispatcher = Director::getInstance()->getEventDispatcher();
+	//Tạo 1 đối tượng lắng nghe sự kiện Chạm màn hình theo cách One by One, xử lý 1 chạm tại 1 thời điểm
 	auto _touchListener = EventListenerTouchOneByOne::create();
-	_touchListener->onTouchBegan = CC_CALLBACK_2(
-		ChemLayer::touch_Kiem, this);
-	_eventDispatcher->addEventListenerWithSceneGraphPriority(_touchListener,
-		this);
+	//Thiết lập "nốt" sự kiện Touch khi xảy ra, ngăn ko cho các đối tượng Bắt sự kiện khác sử dụng event này
+	_touchListener->setSwallowTouches(true);
+	//Bắt sự kiện Touch, khi xảy ra sự kiện Touch nào thì sẽ gọi đến hàm tương ứng của lớp HelloWorld
+	_touchListener->onTouchBegan = CC_CALLBACK_2(ChemLayer::touch_Kiem, this);
+	//Gửi cho dispatcher xử lý
+	dispatcher->addEventListenerWithSceneGraphPriority(_touchListener, this);
+
+	//bat su kien va cham
+	//Tạo đối tượng lắng nghe va chạm nếu xảy ra
+	auto contactListener = EventListenerPhysicsContact::create();
+	//Khi có va chạm sẽ gọi hàm onContactBegin để xử lý va chạm đó, chú ý dòng CC_CALLBACK_1, nhiều tại liệu là CC_CALLBACK_2 sẽ báo lỗi ko chạy
+	contactListener->onContactBegin = CC_CALLBACK_1(ChemLayer::onContactBegin, this);
+	//Bộ truyền tải kết nối với đối tượng bắt va chạm
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(contactListener, this);
 
 	//them am thanh cho game
 	CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic("background-music-aac.wav");
@@ -101,17 +114,18 @@ void ChemLayer::gameLogic(float dt)
 bool ChemLayer::touch_Kiem(Touch* t, Event* e) {
 	//tao sprite kiem
 	katana = Sprite::create("katana.png");  
-	katana->setScale(0.1f);
+	katana->setScale(0.07f);
 	//dat len man hinh
 	katana->setPosition(Vec2(100+player->getContentSize().width,60+player->getContentSize().height/3));
 	//tao body cho kiem
 	auto katanaBody=PhysicsBody::createBox(katana->getBoundingBox().size);
-	katana->setTag(1);
-	katanaBody->setAngularVelocity(-30);
+	katana->setTag(3);
+	//katanaBody->setAngularVelocity(-30);
 	katanaBody->setContactTestBitmask(0x1);
 	//dat khung vat ly vao kiem
 	katana->setPhysicsBody(katanaBody);
 	this->addChild(katana,1);
+
 	return true;
 }
 
@@ -119,7 +133,7 @@ bool ChemLayer::touch_Kiem(Touch* t, Event* e) {
 void ChemLayer::addTarget()
 {
 	auto target = Sprite::create("HaiLua.png");
-	target->setScale(0.1f);
+	target->setScale(0.2f);
 	Size winSize = Director::getInstance()->getWinSize();
 	// Đoạn này tính toán vùng xuất hiện quái sao cho ko bị khuất quái vào viền màn hình
 
@@ -139,8 +153,8 @@ void ChemLayer::addTarget()
 	this->addChild(target,1);
 
 	//Tính toán tốc độ di chuyển của quái
-	int minDuration = (int)2.0;
-	int maxDuration = (int)4.0;
+	int minDuration = (int)4.0;
+	int maxDuration = (int)5.5;
 	int rangeDuration = maxDuration - minDuration;
 	int actualDuration = ( rand() % rangeDuration )
 		+ minDuration;
@@ -161,4 +175,38 @@ void ChemLayer::spriteMoveFinished(Node* sender)
 	// Ép kiểu Contrỏ Sprite của 1 Node*
 	auto sprite = (Sprite *)sender;
 	this->removeChild(sprite, true);    
+}
+
+bool ChemLayer::onContactBegin(const PhysicsContact& contact)
+{
+	//Lấy đối tượng va chạm thứ nhất, ép kiểu con trỏ Sprite*
+	auto bullet = (Sprite*)contact.getShapeA()->getBody()->getNode();
+	////Lấy giá trị cờ để xét xem đối tượng nào ( đạn, quái, hay nhân vật)
+	int tag = bullet->getTag();
+
+	//Lấy đối tượng va chạm thứ hai, ép kiểu con trỏ Sprite*
+	auto target = (Sprite*)contact.getShapeB()->getBody()->getNode();
+	//Lấy giá trị cờ để xét xem đối tượng nào ( đạn, quái, hay nhân vật)
+	int tag1 = target->getTag();
+
+	//Nếu va chạm xảy ra giữa đạn và quái thì xử lý xóa cả đạn và quái khỏi Layer trong Scene ( biến mất khỏi màn)
+	if((tag==2&tag1==3)||(tag==3&tag1==2))
+	{
+
+		this->removeChild(bullet,true); // Xóa đạn
+
+		this->removeChild(target,true); // Xóa quái
+
+	}
+	// Nếu va chạm xảy ra giữa quái và nhân vật thì NV lăn ra chết , rồi GameOver, rồi tính điểm, cái này để bài sau
+	if((tag==1&tag1==2)||(tag==2&tag1==1))
+	{
+		// Xử lý GameOver
+		// Tính điểm 
+		auto gameOverScene = GameOverScene::create(); // Tạo 1 Scene Over của lớp GameOverScene
+		gameOverScene->getLayer()->getLabel()->setString("You Lose :["); // Đặt 1 dòng thông báo lên màn hình
+		Director::getInstance()->replaceScene(gameOverScene); // Thay thế game Scene =  game Over Scene 
+	} 
+
+	return true; // Phải trả lại giá trị true
 }
